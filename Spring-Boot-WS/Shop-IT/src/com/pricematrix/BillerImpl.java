29140c2a661c6bf7c59b6e4aa35a4f0b749b7a51
ctsjava <ctsgit@gmail.com>
2020-04-23 07:34:13 +0530

    Shop-IT app

--- Spring-Boot-WS/Shop-IT/src/com/pricematrix/BillerImpl.java
+++ Spring-Boot-WS/Shop-IT/src/com/pricematrix/BillerImpl.java
@@ -6,31 +6,40 @@ public class BillerImpl {
 	/*
 	 * 	Design Issues
 	 * ----------------
-	 * 1.Too many Dependency object creations. ->
-	 * memory issues -> per issues
+	 * 1.Too many Dependency object creations.
+	 * 			-> memory issues
+	 * 			-> per issues
 	 * 
-	 * Why this issue happened? -------------------------- -> Dependent class itself
-	 * creating Dependency class object
-	 * 
-	 * what is the solution? ---------------------- -> Do not create dependency
-	 * class object in the dependent -> Find the object using JNDI technique
-	 * 
-	 * -> JNDI location is tightly binded with dependent class
-	 * 
-	 * What is the best solution? ---------------------------- -> Do create create,
-	 * do not find, instead ask some one (like containers/providers) -> IoC
-	 * (Inversion of control)
-	 * 
-	 * -> DI (Dependency Injection) -> Injecting/passing dependency object to
-	 * dependent class
-	 * 
-	 * DI can be done with two ways 1. using setter based DI 2. using constructor
-	 * based DI
+	 *  Why this issue happened?
+	 *  --------------------------
+	 *  -> Dependent class itself creating Dependency class object
+	 *  
+	 *  what is the solution?
+	 *  ----------------------
+	 *  -> Do not create dependency class object in the dependent
+	 *  -> Find the object using JNDI technique 
+	 *  
+	 *  -> JNDI location is tightly binded with dependent class
+	 *  
+	 *  What is the best solution?
+	 *  ----------------------------
+	 *  -> Do create create, do not find, instead ask some one (like containers/providers) 
+	 *  -> IoC (Inversion of control)
+	 *  
+	 *  -> DI (Dependency Injection)
+	 *  	-> Injecting/passing dependency object to dependent class
+	 *  
+	 *  DI can be done with two ways
+	 *  1. using setter based DI
+	 *  2. using constructor based DI
 	 */
 
 	private PriceMatrix price;
 
-	
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println(this + " object is garbage collected..");
+	}
 
 	public void setPrice(PriceMatrix price) {
 		this.price = price;
@@ -40,6 +49,7 @@ public class BillerImpl {
 		System.out.println("--- test");
 
 		price.sayHello();
+		
 	}
 
 	public double getCartTotal(String[] cart) { // testing issue
